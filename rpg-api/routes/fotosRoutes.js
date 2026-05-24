5b054374516015006a3c877064b914de77ad7fdd
EnniBr <enzomardesouza@gmail.com>
2026-05-24 00:25:39 -0300

    fix fotosRoutes

--- rpg-api/routes/fotosRoutes.js
+++ rpg-api/routes/fotosRoutes.js
@@ -2,7 +2,7 @@ const express  = require('express')
 const router   = express.Router()
 const multer   = require('multer')
 const path     = require('path')
-const { PrismaClient } = require('../node_modules/@prisma/client')
+const { PrismaClient } = require('@prisma/client')
 const { verificarToken } = require('../middlewares/auth')
 
 const prisma = new PrismaClient()
